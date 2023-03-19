cf378d519f8853306ed7498057226e7c07febd91
Sandipon Paul <psandipon@gmail.com>
2023-03-19 22:14:51 +0100

    Adding Base Input

---
+++ src/common/BaseComponents/BaseInput.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="flex flex-col">
+    <label class="input-label">{{ label }}</label>
+    <div class="number-input">
+      <button class="input-button" @click="decrement">
+        <span> - </span>
+      </button>
+      <input type="number" v-model="inputValue" class="input-box" />
+      <button class="input-button" @click="increment">
+        <span> + </span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps({
+  label: {
+    type: String,
+    required: true
+  },
+  modelValue: {
+    type: Number,
+    required: true
+  }
+})
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: number): void
+}>()
+
+const inputValue = computed({
+  get() {
+    return props.modelValue
+  },
+  set(_value) {
+    emit('update:modelValue', _value)
+  }
+})
+
+const increment = () => {
+  inputValue.value++
+}
+
+const decrement = () => {
+  inputValue.value--
+}
+</script>
+
+<style lang="postcss">
+.input-label {
+  @apply mb-2 font-medium text-gray-700 text-lg capitalize;
+}
+
+.input-button {
+  @apply px-8 py-2 rounded-md text-3xl hover:bg-gray-50 border-none active:bg-gray-100;
+}
+
+.number-input {
+  @apply p-0  bg-gray-50  flex flex-row border-teal-300 rounded-md shadow-md;
+}
+
+.input-box {
+  @apply focus:ring-opacity-0 text-center outline-0 w-full bg-gray-50 text-xl;
+}
+
+input[type='number']::-webkit-inner-spin-button,
+input[type='number']::-webkit-outer-spin-button {
+  opacity: 0;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,36 @@
 <template>
-  <div></div>
+  <div class="app-container">
+    <div class="card title">Mortgage Calculator</div>
+    <hr />
+
+    <div class="card">
+      <BaseInput v-model="_propertyPurchasePrice" label="Property Purchase Price" />
+      <BaseInput v-model="_totalSavings" label="Total Savings" class="mt-6" />
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BaseInput from '@/common/BaseComponents/BaseInput.vue'
+import { ref } from 'vue'
+
+const _totalSavings = ref(0)
+const _propertyPurchasePrice = ref(0)
+</script>
 
 <style>
-.header__logo {
-  display: block;
-  width: 150px;
-  height: 36px;
+.app-container {
+  @apply shadow-lg md:w-32 lg:w-full;
+}
+
+.card {
+  background: rgb(255, 255, 255);
+  padding: 20px;
+  margin: 10px;
+  border: 0px solid black;
+}
+
+.title {
+  @apply text-2xl;
 }
 </style>
