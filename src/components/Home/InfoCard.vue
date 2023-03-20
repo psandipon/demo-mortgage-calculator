34d945d19bcdda61df7d35d967a1c769f1b6ccb8
Sandipon Paul <psandipon@gmail.com>
2023-03-20 02:50:46 +0100

    Adding info card for home page

---
+++ src/components/Home/InfoCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="p-5 shadow-lg rounded-sm bg-gray-100">
+    <span class="">{{ title }}:</span><br />
+    <span class="text-2xl text-teal-900 font-bold">
+      <slot />
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style lang="postcss"></style>
